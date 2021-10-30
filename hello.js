a1b6ab5742bdc26eb85e8ddf7b69b11579281526
Avr8 <42838937+Avr8@users.noreply.github.com>
2021-10-30 22:15:21 +0530

    Update hello.js
    Added Essential Git commands and changed a grammatical error and gave an appropriate title :)

--- hello.js
+++ hello.js
@@ -1,4 +1,12 @@
+// GIT ESSENTIALS
+
 console.log("Hello World!");
-console.log("git add . -> to add all the files to staging are");
+console.log("git add . -> to add all the files to staging area");
 console.log("git commit -> commmit changes to the repository");
 console.log("git push -u origin main -> push the chanegs to the main repository to the main branch");
+
+
+//  Essential Git Commands
+console.log("git config –global user.name “[name]” -> This command sets the author name to be used with your commits.");
+console.log("git init [repository name] ->This command is used to start a new repository.");
+console.log("git clone [url] -> This command is used to obtain a repository from an existing URL.");
