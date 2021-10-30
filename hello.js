87a3de2cc2a84e279ef877ab557ee458e6c639d4
Akshat Gupta <54773056+akshat302@users.noreply.github.com>
2021-10-30 15:50:28 +0530

    Updated Hello.js

--- hello.js
+++ hello.js
@@ -1,1 +1,4 @@
 console.log("Hello World!");
+console.log("git add . -> to add all the files to staging are");
+console.log("git commit -> commmit changes to the repository");
+console.log("git push -u origin main -> push the chanegs to the main repository to the main branch");
